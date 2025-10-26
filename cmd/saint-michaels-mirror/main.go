d49738edbbdf58642bccb26edbee5f9b08b0606e
Your Name <you@example.com>
2025-10-26 16:17:26 -0300

    Fix import order in main.go
    - Sort imports alphabetically
    - Move eventstore/relaystore import to correct position

--- cmd/saint-michaels-mirror/main.go
+++ cmd/saint-michaels-mirror/main.go
@@ -21,9 +21,9 @@ import (
 
 	"github.com/fiatjaf/khatru"
 	"github.com/fiatjaf/khatru/policies"
+	"github.com/girino/nostr-lib/eventstore/relaystore"
 	"github.com/girino/nostr-lib/logging"
 	"github.com/girino/nostr-lib/mirror"
-	"github.com/girino/nostr-lib/eventstore/relaystore"
 	"github.com/nbd-wtf/go-nostr"
 	nip11 "github.com/nbd-wtf/go-nostr/nip11"
 	nip19 "github.com/nbd-wtf/go-nostr/nip19"
